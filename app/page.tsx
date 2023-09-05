82aceb26bef456ddc4a9836f3d34b3268c83daf9
fcvirtual <austin@fcvt.dev>
2023-09-05 11:40:21 -0400

    feat: homepage structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
 export default function Home() {
-  return <main className="text-[yellow]">Hello World</main>;
+  return (
+    <>
+      <head>
+        <h1>HI, I AM AUSTIN</h1>
+        <h2>
+          Professional
+          <br />
+          Web Developer
+          <br /> Based in USA
+        </h2>
+        <h3>
+          Transforming the web one line of code at a time: Crafting cutting-edge
+          digital experiences with precision, passion, and a profound commitment
+          to excellence
+        </h3>
+        <button>My Work</button>
+        <div>austin@fcvt.dev</div>
+      </head>
+      <main>
+        {/* Top Image */}
+        <div></div>
+        {/* Skills */}
+        <div>
+          <h1>My Skills</h1>
+          <div></div>
+        </div>
+        {/* Services */}
+        <h1>What service do I provide</h1>
+        <div></div>
+        {/* Exp */}
+        <div></div>
+      </main>
+    </>
+  );
 }
