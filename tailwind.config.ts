7faed511620cce7f57077be8201ac8bd7ab40d2b
fcvirtual <austin@fcvt.dev>
2023-09-05 11:19:08 -0400

    feat: tailwind colors & font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+/* eslint-disable */
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        blue1: "#0252CD",
+        blue2: "#428DFF",
+        yellow1: "#FFBE62",
+        white1: "#FFFFFF",
+        white2: "#F3F8FF",
+        slate1: "#6F74A7",
+        black1: "#151E2C",
+        black2: "#29374C",
+        grey1: "#778295",
+      },
+      fontFamily: {
+        jakarta: ["Poppins"],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
