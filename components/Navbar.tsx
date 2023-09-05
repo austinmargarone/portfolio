115457c2201aede930d63c3b6e1eff1b4400d584
fcvirtual <austin@fcvt.dev>
2023-09-05 17:46:50 -0400

    fix: Navbar download icon and color

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,8 +8,8 @@ type Props = {};
 const Navbar = (props: Props) => {
   return (
     <main className="my-[1.31rem] flex justify-between md:mb-[4.75rem] md:mt-[2.25rem] ">
-      <div className="ml-[1.5rem] flex h-[1.875rem] w-[1.875rem] shrink-0 gap-[0.52081rem] rounded-[468.75rem] bg-gradient-to-r from-blue to-blue1 p-[0.52rem] md:ml-[5rem] md:h-[2.8125rem] md:w-[2.8125rem] md:gap-[0.78125rem] md:p-[0.78125rem]">
-        <h1 className="md:h-[1.25rem]] flex h-[0.83331rem] w-[0.83331rem] shrink-0 flex-col items-center justify-center text-white md:w-[1.25rem]">
+      <div className="ml-[1.5rem] flex h-[1.875rem] w-[1.875rem] shrink-0 gap-[0.52081rem] rounded-[468.75rem] bg-gradient-to-r from-[#0252cd54] from-20% to-blue p-[0.52rem] md:ml-[5rem] md:h-[2.8125rem] md:w-[2.8125rem] md:gap-[0.78125rem] md:p-[0.78125rem]">
+        <h1 className="flex h-[0.83331rem] w-[0.83331rem] shrink-0 flex-col items-center justify-center text-white md:h-[1.25rem] md:w-[1.25rem]">
           A
         </h1>
       </div>
