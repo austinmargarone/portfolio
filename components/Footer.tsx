a6d8590cbe47444fd0e3284f3b4bb042b2392da2
fcvirtual <austin@fcvt.dev>
2023-09-06 15:55:33 -0400

    feat: global styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <section className="mx-[1.5rem] my-[3rem] flex flex-col justify-between gap-[1rem] border-t-[3px] border-white1 px-[1.5rem] py-[3rem] md:mx-[5.31rem] md:my-[3.25rem] md:flex-row md:p-0">
+    <section className="mx-[1.5rem] mt-[3rem] flex flex-col justify-between gap-[1rem] border-t-[3px] border-white1 px-[1.5rem] py-[3rem] md:mx-[5.31rem] md:mt-[3.25rem] md:flex-row md:p-0">
       <div className="flex content-center justify-center md:pt-[2rem]">
         <h1 className="text-lg leading-[1.8rem] text-grey">
           © 2023 Austin. All rights reserved.
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,15 +10,15 @@ const config: Config = {
   ],
   theme: {
     colors: {
-      'blue': "#0252CD",
-      'blue1': "#428DFF",
-      'yellow': "#FFBE62",
-      'white': "#FFFFFF",
-      'white1': "#F3F8FF",
-      'slate': "#6F74A7",
-      'black': "#151E2C",
-      'black1': "#29374C",
-      'grey': "#778295",
+      blue: "#0252CD",
+      blue1: "#428DFF",
+      yellow: "#FFBE62",
+      white: "#FFFFFF",
+      white1: "#F3F8FF",
+      slate: "#6F74A7",
+      black: "#151E2C",
+      black1: "#29374C",
+      grey: "#778295",
     },
     fontFamily: {
       jakarta: ["Poppins"],
