8c5b0bd354b2466a2bb71b79c02e4d7339c95389
fcvirtual <austin@fcvt.dev>
2023-09-07 14:31:20 -0400

    feat: card demo data

--- components/WorkExpCard.tsx
+++ components/WorkExpCard.tsx
@@ -1,9 +1,25 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
 
 const WorkExpCard = (props: Props) => {
-  return <div>WorkExpCard</div>;
+  return (
+    <div>
+      <div>
+        <Image
+          src="/Rectangle.svg"
+          width={62}
+          height={62}
+          alt="Service Image"
+        />
+      </div>
+      <div>
+        <h4>Slack</h4>
+        <p>Sr. UI Design - 4 Years Experince</p>
+      </div>
+    </div>
+  );
 };
 
 export default WorkExpCard;
