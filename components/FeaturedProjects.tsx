acf652cf3872a1277adf0d966b01a938f48cad18
fcvirtual <austin@fcvt.dev>
2023-09-07 17:00:57 -0400

    feat: Featured Projects setup

--- components/FeaturedProjects.tsx
+++ components/FeaturedProjects.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import FeatProjectsCard from "./FeatProjectsCard";
 
 type Props = {};
 
 const FeaturedProjects = (props: Props) => {
-  return <div>FeaturedProjects</div>;
+  return (
+    <section>
+      <div>
+        <div>
+          <h2>Featured Projects</h2>
+          <div>
+            <FeatProjectsCard />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FeaturedProjects;
