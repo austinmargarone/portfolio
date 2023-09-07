0df5c08c1b2e8be077da849c5c38af85dce4d829
fcvirtual <austin@fcvt.dev>
2023-09-07 16:47:09 -0400

    feat: Featured Projects init

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import FeaturedProjects from "@/components/FeaturedProjects";
 import Hero from "@/components/Hero";
-import ServiceCard from "@/components/ServiceCard";
-import ServiceCardWhite from "@/components/ServiceCardWhite";
-import SkillTile from "@/components/SkillTile";
 
 export default function Home() {
   return (
@@ -10,24 +8,16 @@ export default function Home() {
         <div className="bg-white1 md:pb-[6.01rem]">
           <Hero />
         </div>
-        <button>My Work</button>
       </main>
       <section>
         {/* Top Image */}
         <div></div>
         {/* Skills */}
         <div>
-          <h2>My Skills</h2>
-          <div>
-            <SkillTile imageUrl="" />
-          </div>
+          <div></div>
         </div>
         {/* Services */}
-        <h2>What service do I provide</h2>
-        <div>
-          <ServiceCard />
-          <ServiceCardWhite />
-        </div>
+        <div></div>
         {/* Exp */}
         <div>
           <div></div>
@@ -35,13 +25,12 @@ export default function Home() {
         </div>
         {/* Projects */}
         <div>
-          <h2>Featured Projects</h2>
-          <div></div>
+          <div>
+            <FeaturedProjects />
+          </div>
         </div>
         {/* About */}
-        <div>
-          <h2>What they say about me</h2>
-        </div>
+        <div></div>
         {/* Contact */}
         <div></div>
       </section>
---
+++ components/FeatProjectsCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const FeatProjectsCard = (props: Props) => {
+  return <div>FeatProjectsCard</div>;
+};
+
+export default FeatProjectsCard;
