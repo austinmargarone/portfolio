5de640d9e1bcc91a056be343cf019292366e455f
fcvirtual <austin@fcvt.dev>
2023-09-08 11:27:36 -0400

    feat: commonent added to homepage

--- components/homepage/FeaturedProjects.tsx
+++ components/homepage/FeaturedProjects.tsx
@@ -1,16 +1,36 @@
 import React from "react";
 import FeatProjectsCard from "./FeatProjectsCard";
+import Image from "next/image";
 
 type Props = {};
 
 const FeaturedProjects = (props: Props) => {
   return (
-    <section className=" 2xl:mx-auto 2xl:max-w-[120rem] mx-[1.5rem] flex w-full max-w-[28rem] flex-col justify-center sm:mx-auto sm:max-w-[30rem] md:mx-auto md:max-w-[60rem] md:flex-row lg:mx-auto lg:max-w-[75rem] xl:mx-auto xl:max-w-[90rem]">
-      <div>
-        <div>
-          <h2>Featured Projects</h2>
-          <div>
+    <section className=" 2xl:mx-auto 2xl:max-w-[120rem] mx-auto flex w-full max-w-[28rem] flex-col justify-center sm:mx-auto sm:max-w-[30rem] md:mx-auto md:max-w-[60rem] md:flex-row lg:mx-auto lg:max-w-[75rem] xl:mx-auto xl:max-w-[90rem]">
+      <div className="my-[3rem] md:my-[4.5rem]">
+        <div className="flex flex-col gap-[2.25rem] md:gap-[3rem]">
+          <h2 className="">Featured Projects</h2>
+          <div className="flex flex-col gap-[2.25rem] md:gap-[3rem]">
             <FeatProjectsCard />
+            <FeatProjectsCard />
+            <FeatProjectsCard />
+          </div>
+          <div>
+            <div className="hidden justify-center md:flex">
+              <button className="projectbutton1 flex">
+                <p className="boldbody flex text-white">
+                  See more case studies
+                </p>
+                <div className="flex shrink-0">
+                  <Image
+                    src="/Arrow1.svg"
+                    width={16}
+                    height={0}
+                    alt="Arrow Right"
+                  />
+                </div>
+              </button>
+            </div>
           </div>
         </div>
       </div>
