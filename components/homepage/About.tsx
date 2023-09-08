e354e4bf67d49659886d11e4fbe3ba1d69b39384
fcvirtual <austin@fcvt.dev>
2023-09-08 13:47:26 -0400

    feat: About desktop

--- components/homepage/About.tsx
+++ components/homepage/About.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="about md:aboutlg my-[3rem] bg-white1">
+    <div className="about md:aboutlg mx-auto my-[3rem] bg-white1">
       <div>
         <div>
           <h2>What they say about me</h2>
@@ -27,30 +27,28 @@ const About = () => {
                 src="/arrow-left.svg"
                 width={20}
                 height={20}
-                alt="Headshot"
+                alt="Arrow Left"
               />
             </div>
             <div className="circle">
-              {" "}
               <Image
                 className="shrink-0 rounded-[1rem]"
                 src="/arrow-right.svg"
                 width={20}
                 height={20}
-                alt="Headshot"
+                alt="Arrow Right"
               />
             </div>
           </div>
         </div>
-
         <div>
-          <div className="mb-[1rem]">
+          <div className="mb-[1rem] mt-[2.5rem]">
             <Image
               className="shrink-0 rounded-[1rem]"
               src="/Stars.svg"
               width={200}
               height={200}
-              alt="Headshot"
+              alt="Stars"
             />
           </div>
           <div>
@@ -72,42 +70,64 @@ const About = () => {
           </div>
         </div>
       </div>
-      <div className="hidden md:block">
-        <div>
-          <div className="mb-[1rem]">
+      <div className="mt-[4rem] hidden md:block">
+        <div className="flex">
+          <div className="circlelg mt-[6rem] flex">
             <Image
               className="shrink-0 rounded-[1rem]"
-              src="/Stars.svg"
-              width={200}
-              height={200}
-              alt="Headshot"
+              src="/arrow-left.svg"
+              width={24}
+              height={24}
+              alt="Arrow Left"
             />
           </div>
-          <div className="mb-[1rem]">
+          <div className="flex">
+            <div className="mx-[1.62rem]">
+              <Image
+                className="shrink-0 rounded-[1rem]"
+                src="/headshot.svg"
+                width={800}
+                height={800}
+                alt="Headshot"
+              />
+            </div>
+            <div>
+              <div className="mb-[1rem]">
+                <Image
+                  className="shrink-0 rounded-[1rem]"
+                  src="/Stars.svg"
+                  width={116}
+                  height={20}
+                  alt="Stars"
+                />
+              </div>
+              <div>
+                <p className="regbase mb-[2rem] text-slate">
+                  I have had the pleasure of working with Adrian as a Software
+                  Developer and can confidently say that he is a valuable asset
+                  to any team. He is highly skilled in ReactJS, NextJS,
+                  Javascript, Material UI, Tailwind CSS, Redux, and GitHub, and
+                  consistently delivers high-quality code.
+                </p>
+              </div>
+              <div>
+                <p className="boldbody mb-[.25rem] self-stretch text-black">
+                  - Austin Margarone
+                </p>
+                <p className="regbody self-stretch text-slate	">
+                  Managing-Member of FC Virtual Tours
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="circlelg mt-[6rem]">
             <Image
               className="shrink-0 rounded-[1rem]"
-              src="/Stars.svg"
-              width={200}
-              height={200}
-              alt="Headshot"
+              src="/arrow-right.svg"
+              width={24}
+              height={24}
+              alt="Arrow Right"
             />
-          </div>
-          <div>
-            <p className="regbody mb-[1.5rem] self-stretch text-black">
-              I have had the pleasure of working with Adrian as a Software
-              Developer and can confidently say that he is a valuable asset to
-              any team. He is highly skilled in ReactJS, NextJS, Javascript,
-              Material UI, Tailwind CSS, Redux, and GitHub, and consistently
-              delivers high-quality code.
-            </p>
-          </div>
-          <div>
-            <p className="boldbody self-stretch text-black	">
-              - Austin Margarone
-            </p>
-            <p className="regbody self-stretch text-slate	">
-              Managing-Member of FC Virtual Tours
-            </p>
           </div>
         </div>
       </div>
