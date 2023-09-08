45dcf7eab7d0440842c1814330059c86b5a7078a
fcvirtual <austin@fcvt.dev>
2023-09-08 11:10:54 -0400

    feat: desktop component

--- components/homepage/FeatProjectsCard.tsx
+++ components/homepage/FeatProjectsCard.tsx
@@ -6,11 +6,11 @@ type Props = {};
 const FeatProjectsCard = (props: Props) => {
   return (
     <div className="flex h-[32.125rem] w-[21.6rem] flex-col rounded-[1rem] bg-slate md:h-[27.75rem] md:w-[79.375rem] md:flex-row">
-      <div className="ml-[1.5rem] flex flex-col">
-        <h3 className="projecttitle mr-[1.63rem] mt-[2.5rem] w-[18.4375rem] text-left text-white md:w-[28.25rem]">
+      <div className="ml-[1.5rem] flex flex-col md:ml-[6.75rem]">
+        <h3 className="projecttitle mr-[1.63rem] mt-[2.5rem] w-[18.4375rem] text-left text-white md:mt-[6.06rem] md:w-[28.25rem]">
           Morrent - A Car Rental Application
         </h3>
-        <div className="mb-[1.88rem]  mt-[1.5rem] flex gap-[1.25rem]">
+        <div className="my-[1.88rem] flex gap-[1.25rem] md:mb-[3.13rem] md:mt-[1.44rem]">
           <button className="projectbutton flex bg-yellow">
             <p className="smallreg text-white">ReactJS, Next.js</p>
           </button>
@@ -31,8 +31,16 @@ const FeatProjectsCard = (props: Props) => {
           />
         </div>
       </div>
-      <div className="mx-auto mt-[1.49rem] flex flex-col">
-        <div className="">
+      <div className="mx-auto mt-[1.49rem] flex flex-col md:ml-[2.27rem]">
+        <div className="my-auto hidden md:flex">
+          <Image
+            src="/Display.svg"
+            width={701}
+            height={330}
+            alt="Computer & phone"
+          />
+        </div>
+        <div className="flex md:hidden">
           <Image
             src="/Display.svg"
             width={325}
