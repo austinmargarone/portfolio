32a1333170888217d74cdf7f8e0932770a4272d5
Austin Margarone <123497938+austinmargarone@users.noreply.github.com>
2023-09-08 13:51:02 -0400

    Update components/homepage/FeaturedProjects.tsx
    Co-authored-by: Brandon Etter <brandonetter@gmail.com>

--- components/homepage/FeaturedProjects.tsx
+++ components/homepage/FeaturedProjects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import FeatProjectsCard from "./FeatProjectsCard";
 import Image from "next/image";
+
+import FeatProjectsCard from "./FeatProjectsCard";
 
 
 const FeaturedProjects = () => {
