ac497ccd8f37d83ba599a4c925fb59d5606676de
fcvirtual <austin@fcvt.dev>
2023-09-11 16:59:41 -0400

    fix: breakpoint & add md breakpoint

--- components/homepage/FeaturedProjects.tsx
+++ components/homepage/FeaturedProjects.tsx
@@ -5,7 +5,7 @@ import FeatProjectsCard from "./FeatProjectsCard";
 
 const FeaturedProjects = () => {
   return (
-    <section className=" mx-auto flex w-fit flex-col justify-center md:flex-row xl:max-w-[120rem]">
+    <section className=" mx-auto flex w-fit flex-col justify-center pt-[3rem] md:max-w-[60rem] md:flex-row xl:max-w-[120rem]">
       <div className="md:gap-[3rem]my-[3rem] flex flex-col gap-[2.25rem] md:my-[4.5rem]">
         <h2 className="">Featured Projects</h2>
         <div className="flex flex-col gap-[2.25rem] md:gap-[3rem]">
--- components/homepage/Services.tsx
+++ components/homepage/Services.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 const Services = (props: Props) => {
   return (
-    <section className="services md:serviceslg mx-auto mb-[3rem] flex w-fit flex-col justify-center py-[3rem] md:mb-[4.5rem]  xl:max-w-[100rem]">
+    <section className="services md:serviceslg mx-auto mb-[3rem] flex w-fit flex-col justify-center py-[3rem] md:mb-[4.5rem]  md:max-w-[60rem] xl:max-w-[100rem]">
       <div className="services1 md:services1lg flex flex-col md:w-fit">
         <h2>What service do I provide</h2>
         <div className="flex flex-col justify-center gap-[2.5rem] md:flex-row md:flex-wrap">
