50d29a5000bb18042fd7d4bbca85cf247f8d2b9e
fcvirtual <austin@fcvt.dev>
2023-09-11 15:43:02 -0400

    fix: featured mx

--- components/homepage/FeatProjectsCard.tsx
+++ components/homepage/FeatProjectsCard.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 const FeatProjectsCard = (props: Props) => {
   return (
-    <div className="flex h-[32.125rem] w-[21.6rem] flex-col rounded-[1rem] bg-slate md:h-[27.75rem] md:w-[79.375rem] md:flex-row">
+    <div className="mx-auto flex h-[32.125rem] w-[21.6rem] flex-col rounded-[1rem] bg-slate md:h-[27.75rem] md:w-[79.375rem] md:flex-row">
       <div className="ml-[1.5rem] flex flex-col md:ml-[6.75rem]">
         <h3 className="projecttitle mr-[1.63rem] mt-[2.5rem] w-[18.4375rem] text-left text-white md:mt-[6.06rem] md:w-[28.25rem]">
           Morrent - A Car Rental Application
