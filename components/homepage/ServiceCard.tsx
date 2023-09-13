2c0d0098a5f72c4048a13dfdec11ded58593c72a
fcvirtual <austin@fcvt.dev>
2023-09-13 12:08:59 -0400

    feat: services

--- components/homepage/ServiceCard.tsx
+++ components/homepage/ServiceCard.tsx
@@ -1,26 +1,26 @@
 import Image from "next/image";
+import { title } from "process";
 import React from "react";
 
-type Props = {};
+type Props = { title: string; body: string; image: string };
 
 const ServiceCard = (props: Props) => {
   return (
     <div className="flex h-[16.66em] w-[18rem] flex-col rounded-[0.625rem] bg-white1 px-[1.56rem] shadow-[0px_1px_20px_0px_rgba(224,236,255)] dark:bg-darkbg dark:shadow-[0px_1px_20px_0px_rgba(31,46,72)]">
-      <div className="mb-[2.25rem] mt-[2.5rem] flex">
+      <div className="mb-[2.25rem] mt-[2.5rem] flex h-[3.875rem] w-[3.875rem] justify-center rounded-[0.625rem] bg-blue shadow-[18px_18px_26px_0px_rgba(2,82,205,0.28)]">
         <Image
-          className="rounded-[0.625rem] bg-blue shadow-[18px_18px_26px_0px_rgba(2,82,205,0.28)]"
-          src=""
-          width={62}
-          height={62}
+          className="flex"
+          src={props.image}
+          width={24}
+          height={24}
           alt="Service Image"
         />
       </div>
       <h1 className="mb-[.62rem] text-2xl	font-semibold	leading-[1.95rem] text-black">
-        Front-End Dev
+        {props.title}
       </h1>
       <p className="text-sm font-normal leading-[1.35625rem] text-slate">
-        Front-end dev includes creating the user interface and interactive
-        elements of websites and apps.
+        {props.body}
       </p>
     </div>
 
--- components/homepage/Services.tsx
+++ components/homepage/Services.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ServiceCard from "./ServiceCard";
 
-type Props = {};
+type Props = { title: string; body: string; image: string };
 
 const Services = (props: Props) => {
   return (
@@ -12,10 +12,34 @@ const Services = (props: Props) => {
           Provide
         </h2>
         <div className="flex flex-col justify-center gap-[2.5rem] md:flex-row md:flex-wrap">
-          <ServiceCard />
-          <ServiceCard />
-          <ServiceCard />
-          <ServiceCard />
+          <ServiceCard
+            title={"Analytics & SEO"}
+            body={
+              "Implement tracking & analytics tools to monitor website performance & improve SEO."
+            }
+            image={"/seo.svg"}
+          />
+          <ServiceCard
+            title={"Front-End Dev"}
+            body={
+              "Front-end dev includes creating the user interface and interactive elements of websites and apps."
+            }
+            image={"/Pointer.svg"}
+          />
+          <ServiceCard
+            title={"Back-End Dev"}
+            body={
+              "Front-end includes creating the user interface and interactive elements of websites and apps."
+            }
+            image={"/Code.svg"}
+          />
+          <ServiceCard
+            title={"Web Optimization"}
+            body={
+              "Web optimization focuses on improving website performance for a smoother user experience"
+            }
+            image={"/Level.svg"}
+          />
         </div>
       </div>
     </section>
