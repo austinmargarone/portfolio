bc74dd0484a9219c39c14a453c2c50d046b528ba
fcvirtual <austin@fcvt.dev>
2023-09-13 09:58:32 -0400

    fix headings

--- components/homepage/Services.tsx
+++ components/homepage/Services.tsx
@@ -8,8 +8,8 @@ const Services = (props: Props) => {
     <section className="services md:serviceslg mx-auto mb-[3rem] flex w-fit flex-col justify-center py-[3rem] md:mb-[4.5rem]  md:max-w-[60rem] xl:max-w-[100rem]">
       <div className="services1 md:services1lg flex flex-col md:w-fit">
         <h2 className="dark:text-white">
-          What <span className="underline-magic w-fit">service</span> do I
-          provide
+          What <span className="underline-magic w-fit">Services</span> Do I
+          Provide
         </h2>
         <div className="flex flex-col justify-center gap-[2.5rem] md:flex-row md:flex-wrap">
           <ServiceCard />
