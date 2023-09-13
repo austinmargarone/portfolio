4e5df1e80fcbee5a026a46770bb4667293e92ee7
fcvirtual <austin@fcvt.dev>
2023-09-13 12:17:36 -0400

    feat: skills

--- components/homepage/MySkills.tsx
+++ components/homepage/MySkills.tsx
@@ -11,18 +11,22 @@ const MySkills = (props: Props) => {
           My Skills
         </h2>
         <div className="tech md:techlg flex md:flex-row">
-          <SkillTile imageUrl="" />
-          <SkillTile imageUrl="" />
-          <SkillTile imageUrl="" />
-          <SkillTile imageUrl="" />
-          <SkillTile imageUrl="" />
-          <SkillTile imageUrl="" />
-          <SkillTile imageUrl="" />
-          <SkillTile imageUrl="" />
-          <SkillTile imageUrl="" />
-          <SkillTile imageUrl="" />
-          <SkillTile imageUrl="" />
-          <SkillTile imageUrl="" />
+          <SkillTile imageUrl="/js.svg" />
+          <SkillTile imageUrl="/react.svg" />
+          <SkillTile imageUrl="/Redux.svg" />
+          <SkillTile imageUrl="Next.svg" />
+          <SkillTile imageUrl="ts.svg" />
+          <SkillTile imageUrl="html.svg" />
+          <SkillTile imageUrl="css.svg" />
+          <SkillTile imageUrl="sass.svg" />
+          <SkillTile imageUrl="Material.svg" />
+          <SkillTile imageUrl="tailwind.svg" />
+          <SkillTile imageUrl="Motion.svg" />
+          <SkillTile imageUrl="git.svg" />
+          <SkillTile imageUrl="github.svg" />
+          <SkillTile imageUrl="nodejs.svg" />
+          <SkillTile imageUrl="ex.svg" />
+          <SkillTile imageUrl="mongodb.svg" />
         </div>
       </div>
     </section>
