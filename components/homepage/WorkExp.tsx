5d990193fbd0b35eb6c0075df5248731c1af5561
fcvirtual <austin@fcvt.dev>
2023-09-19 14:03:42 -0400

    refactor: work exp breakpoints

--- components/homepage/WorkExp.tsx
+++ components/homepage/WorkExp.tsx
@@ -6,13 +6,13 @@ type Props = {};
 
 const WorkExp = (props: Props) => {
   return (
-    <section className="mx-auto flex justify-center pb-[3rem] md:max-w-[60rem] xl:max-w-[80rem]">
+    <section className="mx-auto flex justify-center pb-[3rem] xs:max-w-[21.5rem] ss:max-w-[28rem] sm:max-w-[40rem] md:max-w-[60rem] xl:max-w-[80rem]">
       <div className="flex flex-col justify-center gap-[1rem] lg:flex-row">
-        <div className="flex w-[21.5625rem] flex-col content-center justify-center rounded-[1rem] bg-black px-[2.38rem] pb-[2.39rem] pt-[3.68rem] dark:bg-white1 md:w-auto md:pb-[6.5625rem] md:pl-[6.56rem] md:pr-[8.94rem] md:pt-[9.5rem]">
+        <div className="flex w-fit flex-col content-center justify-center rounded-[1rem] bg-black px-[2.38rem] pb-[2.39rem] pt-[3.68rem] dark:bg-white1 md:w-auto md:pb-[6.5625rem] md:pl-[6.56rem] md:pr-[8.94rem] md:pt-[9.5rem]">
           <h2 className="mb-[1.49rem] flex h-[5.9rem] w-[12.625rem] shrink-0 flex-col text-left text-white dark:text-black md:h-[7.31rem] md:w-[20rem]">
             Work <span className="underline-magicexp w-fit">Experience</span>
           </h2>
-          <p className="smallreg mb-[2.66rem] h-[4.1rem] w-[16.1875rem] shrink-0 text-white dark:text-black md:mb-[3.66rem] md:w-[20rem]">
+          <p className="smallreg mb-[2.66rem] h-[4.1rem] shrink-0 text-white dark:text-black md:mb-[3.66rem] md:w-[20rem]">
             Progress and milestones: A simple walkthrough of roles, projects,
             and achievements in my career.
           </p>
