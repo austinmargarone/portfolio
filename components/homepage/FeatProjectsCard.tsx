b87ed2877d4269b7dc46a9f50b4d0626923d60e0
fcvirtual <austin@fcvt.dev>
2023-09-19 15:17:11 -0400

    refactor: featured card & breakpoint:

--- components/homepage/FeatProjectsCard.tsx
+++ components/homepage/FeatProjectsCard.tsx
@@ -17,7 +17,7 @@ const FeatProjectsCard = (props: Props) => {
       className={`${bgColor} mx-auto flex h-[32.125rem] w-full flex-col rounded-[1rem] xs:max-w-[21.5rem] ss:max-w-[25rem] sm:max-w-[40rem] md:h-[27.75rem] md:w-full md:max-w-[60rem] md:flex-row xl:max-w-[80rem]`}
     >
       <div className="ml-[1.5rem] flex flex-col md:ml-[6.75rem] ">
-        <h3 className="projecttitle mr-[1.63rem] mt-[2.5rem] w-[18.4375rem] text-left text-white md:mt-[6.06rem] md:w-[23.25rem]">
+        <h3 className="projecttitle md:projecttitlelg mr-[1.63rem] mt-[2.5rem] w-[80%] text-left text-white md:mt-[4.06rem] md:w-[20rem] xl:mt-[6.06rem] xl:w-[28.25rem]">
           {props.title}
         </h3>
         <div className="my-[1.88rem] flex gap-[1.25rem] md:mb-[3.13rem] md:mt-[1.44rem]">
@@ -44,7 +44,7 @@ const FeatProjectsCard = (props: Props) => {
         </div>
       </div>
       <div className="mx-auto mt-[1.49rem] flex flex-col overflow-hidden md:ml-[2.27rem] md:mt-[7rem] xl:mt-[3.88rem]">
-        <div className="relative left-1 hidden w-auto max-w-[695px] md:flex">
+        <div className="relative left-9 hidden md:flex">
           <Image
             src="/Display.svg"
             width={701}
