b2952137181b9972d0dfcc4e7ab9dc577c7a7bcb
fcvirtual <austin@fcvt.dev>
2023-09-20 16:20:57 -0400

    fix: Design image _ Still needs work

--- components/projects/Design.tsx
+++ components/projects/Design.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
@@ -6,14 +7,19 @@ const Design = (props: Props) => {
   return (
     <section className="w-full">
       <div className="bg-blue px-[8.56rem] py-[.61rem] dark:bg-blue1">
-        <h4 className="smallbold md:ptitle2 flex justify-center uppercase text-white">
+        <h4 className="smallbold md:ptitle2 flex w-full justify-center uppercase text-white">
           High-Fidelity Figma Design
         </h4>
-        <div className="h-[13.08294rem] w-full">
-          {/* <Image src="/Figma.svg" fill={true} alt="Computer" /> */}
-        </div>
       </div>
-      <div></div>
+      <div>
+        <Image
+          src="/Figma.svg"
+          width={393}
+          height={209}
+          className="h-[209px] w-full grow object-contain md:h-[767px]"
+          alt="Computer"
+        />
+      </div>
     </section>
   );
 };
