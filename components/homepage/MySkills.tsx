f9b815a6d9b1c2635744544f199e8b1a5db046d3
fcvirtual <austin@fcvt.dev>
2023-09-21 14:24:27 -0400

    feat: SVGR Images

--- components/homepage/MySkills.tsx
+++ components/homepage/MySkills.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import SkillTile from "./SkillTile";
-import { CSSIcon, HTMLIcon, TSIcon } from "@/components/svg";
 
 type Props = {};
 
@@ -28,9 +27,6 @@ const MySkills = (props: Props) => {
           <SkillTile imageUrl="nodejs.svg" />
           <SkillTile imageUrl="ex.svg" />
           <SkillTile imageUrl="mongodb.svg" />
-        </div>
-        <div className="h-[10rem] w-[10rem] fill-yellow">
-          <CSSIcon className="h-[10rem] w-[10rem] fill-yellow" />
         </div>
       </div>
     </section>
