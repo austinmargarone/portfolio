89f436be445516314c3345f86378519655369be0
fcvirtual <austin@fcvt.dev>
2023-09-21 17:52:30 -0400

    feat: Skill tile hover

--- components/homepage/SkillTile.tsx
+++ components/homepage/SkillTile.tsx
@@ -1,25 +1,23 @@
 import React from "react";
-import Image from "next/image";
 
-type Props = {
-  imageUrl: string;
-};
-
-const SkillTile = (props: Props) => {
+const SkillTile = ({ StackImage }: { StackImage: any }) => {
   return (
-    // Change bg to fill when implemented
-    <div className="flex h-[3.3rem] w-[3.3rem] shrink-0 justify-center rounded-full bg-white1 hover:scale-125 hover:border hover:border-grey hover:bg-white hover:fill-yellow dark:bg-darkbg hover:dark:bg-darkbg2 md:h-[6.25rem] md:w-[6.25rem]">
-      <div className="hidden shrink-0 md:flex">
-        <Image
-          className="hover:fill-yellow"
-          src={props.imageUrl}
+    <div className="group flex h-[3.3rem] w-[3.3rem] shrink-0 justify-center rounded-full bg-white1 hover:scale-125 hover:border hover:border-grey  hover:bg-white dark:bg-darkbg hover:dark:bg-darkbg2 md:h-[6.25rem] md:w-[6.25rem]">
+      <div className="m-auto hidden shrink-0 md:flex">
+        <StackImage
+          className="justify-center group-hover:fill-blue dark:fill-grey"
           width={50}
           height={50}
           alt="Stack Image"
         />
       </div>
-      <div className="flex shrink-0 md:hidden">
-        <Image src={props.imageUrl} width={25} height={25} alt="Stack Image" />
+      <div className="m-auto flex shrink-0 md:hidden">
+        <StackImage
+          className="group-hover:fill-blue dark:fill-grey"
+          width={25}
+          height={25}
+          alt="Stack Image"
+        />
       </div>
     </div>
   );
