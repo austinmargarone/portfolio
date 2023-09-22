80db0c5a938e3463027e6d4771e42a76143a2d3a
fcvirtual <austin@fcvt.dev>
2023-09-22 13:23:38 -0400

    feat: WorkExp Animation

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = (props: Props) => {
     <section className="mx-[1.5rem] flex flex-col justify-between gap-[1rem] border-t-[3px] border-white1 px-[1.5rem] py-[3rem] md:mx-auto md:mt-[3.25rem] md:max-w-[60rem] md:flex-row md:p-0 md:py-[3.5rem] xl:max-w-[80rem]">
       <div className="flex content-center justify-center md:pt-[2rem]">
         <h1 className="text-lg leading-[1.8rem] text-grey">
-          © 2023 Flower City Virtual Tours LLC. All rights reserved.
+          © 2023 Austin All rights reserved.
         </h1>
       </div>
       <div className="flex content-center justify-center gap-[1.5rem]  md:pt-[2rem]">
