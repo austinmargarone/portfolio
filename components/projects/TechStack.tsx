de494f0d76fbd64b054b6b88b747ef3d58a2d1ec
fcvirtual <austin@fcvt.dev>
2023-09-28 15:51:04 -0400

    map over tech images

--- components/projects/TechStack.tsx
+++ components/projects/TechStack.tsx
@@ -1,23 +1,7 @@
 import React from "react";
-import SkillTile from "./SkillTile";
-import {
-  ReactIcon,
-  ReduxIcon,
-  NextIcon,
-  TSIcon,
-  HTMLIcon,
-  CSSIcon,
-  SassIcon,
-  TailwindIcon,
-  MotionIcon,
-  GitIcon,
-  GitHubIcon,
-  NodeIcon,
-  ExpressIcon,
-  MongodbIcon,
-} from "../svg";
+import Image from "next/image";
 
-type Props = { tech: any };
+type Props = { tech: string };
 
 const TechStack = (props: Props) => {
   return (
@@ -31,21 +15,30 @@ const TechStack = (props: Props) => {
         </h4>
       </div>
       <div className="tiles flex">
+        <div className="group flex h-[3.3rem] w-[3.3rem] shrink-0 justify-center rounded-full bg-white1 hover:scale-125 hover:border hover:border-grey  hover:bg-white dark:bg-darkbg hover:dark:bg-darkbg2 md:h-[6.25rem] md:w-[6.25rem]">
+          <div className="m-auto hidden shrink-0 md:flex">
+            <Image
+              className="justify-center	 grayscale group-hover:grayscale-0"
+              src={props.tech[0]}
+              width={50}
+              height={50}
+              alt="Stack Image"
+            />
+          </div>
+          <div className="m-auto flex shrink-0 md:hidden">
+            <Image
+              className="justify-center	 grayscale group-hover:grayscale-0"
+              src={props.tech[0]}
+              width={25}
+              height={25}
+              alt="Stack Image"
+            />
+          </div>
+          {/* {props.map((props, index) => (
+            <SkillTile {...props} key={props.tech} />
+          ))} */}
+        </div>
         {/* <SkillTile tech={props.tech} /> */}
-        <SkillTile StackImage2={ReactIcon} />
-        <SkillTile StackImage2={ReduxIcon} />
-        <SkillTile StackImage2={NextIcon} />
-        <SkillTile StackImage2={TSIcon} />
-        <SkillTile StackImage2={HTMLIcon} />
-        <SkillTile StackImage2={CSSIcon} />
-        <SkillTile StackImage2={SassIcon} />
-        <SkillTile StackImage2={TailwindIcon} />
-        <SkillTile StackImage2={MotionIcon} />
-        <SkillTile StackImage2={GitIcon} />
-        <SkillTile StackImage2={GitHubIcon} />
-        <SkillTile StackImage2={NodeIcon} />
-        <SkillTile StackImage2={ExpressIcon} />
-        <SkillTile StackImage2={MongodbIcon} />
       </div>
     </section>
   );
