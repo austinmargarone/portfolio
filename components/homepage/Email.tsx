0d866ef48b5da195c85a917e36edb7d6158b6fa1
fcvirtual <austin@fcvt.dev>
2024-01-30 15:10:11 -0500

    feat: Toast

--- components/homepage/Email.tsx
+++ components/homepage/Email.tsx
@@ -1,15 +1,24 @@
 "use client";
 import Image from "next/image";
 import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Email = () => {
   function copyText(entryText: string) {
     navigator.clipboard.writeText(entryText);
   }
+
+  const notify = () => toast("Email copied to clipboard!");
+
   return (
     <>
+      <ToastContainer />
       <button
-        onClick={() => copyText("austin@fcvt.dev")}
+        onClick={() => {
+          copyText("austin@fcvt.dev");
+          notify();
+        }}
         className="herocopy md:herocopylg mb-[1.69rem] flex bg-white hover:shadow-lg dark:bg-darkbg2 md:mb-0 md:w-fit"
       >
         <p className="herocopytext text-slate dark:text-white1">
