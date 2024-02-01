048d5a59e2e196876370a7156f8a9bc067469ecc
fcvirtual <austin@fcvt.dev>
2024-02-01 10:40:04 -0500

    feat: Homepage Width

--- components/homepage/WorkExpCard.tsx
+++ components/homepage/WorkExpCard.tsx
@@ -5,11 +5,11 @@ type Props = { title: string; body: string; image: string };
 
 const WorkExpCard = (props: Props) => {
   return (
-    <section className="expcard md:expcardlg mx-auto flex h-[13.125rem] w-[18.5rem] flex-col shadow-[-25px_47px_66px_0px_rgba(0,0,0,0.08)] hover:shadow-[-25px_47px_66px_0px_rgba(0,0,0,0.08)] dark:border-white1 dark:bg-grey dark:hover:shadow-[-25px_47px_66px_0px_rgba(224,236,255)] xs:w-full sm:w-full md:h-[7.625rem] md:w-[37.9375rem] md:flex-row md:shadow-none md:hover:translate-x-6 lg:max-w-[32rem] xl:max-w-full">
-      <div className="ml-[2.25rem] mt-[2.25rem] flex h-[3.625rem] w-[3.625rem] shrink md:m-[2rem]">
+    <section className="expcard md:expcardlg mx-auto flex h-[13.125rem] w-[20rem] flex-col shadow-[-25px_47px_66px_0px_rgba(0,0,0,0.08)] hover:shadow-[-25px_47px_66px_0px_rgba(0,0,0,0.08)] dark:border-white1 dark:bg-grey dark:hover:shadow-[-25px_47px_66px_0px_rgba(224,236,255)] xs:w-full sm:w-full md:h-[7.625rem] md:w-[37.9375rem] md:flex-row md:shadow-none md:hover:translate-x-6 lg:max-w-[32rem] xl:max-w-full">
+      <div className="ml-[1rem] mt-[2.25rem] flex h-[3.625rem] w-[3.625rem] shrink sm:ml-[2.25rem] md:m-[2rem]">
         <Image src={props.image} width={62} height={62} alt="Service Image" />
       </div>
-      <div className="mb-[2.25rem] ml-[2.25rem] flex flex-col md:my-[1.87rem] md:ml-0">
+      <div className="mb-[2.25rem] ml-[1rem] flex flex-col sm:ml-[2.25rem] md:my-[1.87rem] md:ml-0">
         <h4 className="mb-[.56rem] mt-[1.13rem] font-bold dark:text-white md:mt-0">
           {props.title}
         </h4>
