ff3d7e8a55f2d044d669a93e064cb385c3fa5e34
fcvirtual <austin@fcvt.dev>
2024-02-01 11:40:55 -0500

    feat: Implement Toast

--- components/Download.tsx
+++ components/Download.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Resume } from "./svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Download: React.FC = () => {
+  const notify = () => toast("Downloaded Austin's Resume!");
+
   const downloadResume = () => {
     const link = document.createElement("a");
     link.href = `${window.location.origin}/AustinsResume.pdf`;
@@ -13,23 +17,29 @@ const Download: React.FC = () => {
 
     // Remove the link from the document
     document.body.removeChild(link);
+
+    // Show a toast notification
+    notify();
   };
 
   return (
-    <button
-      onClick={downloadResume}
-      className="flex content-center gap-[.1875rem]"
-    >
-      <Resume
-        className="fill-slate dark:fill-white1"
-        width={20}
-        height={20}
-        alt="Download"
-      />
-      <p className="text-[1rem] font-semibold not-italic text-slate dark:text-white1">
-        Resume
-      </p>
-    </button>
+    <>
+      <ToastContainer />
+      <button
+        onClick={downloadResume}
+        className="flex content-center gap-[.1875rem]"
+      >
+        <Resume
+          className="fill-slate dark:fill-white1"
+          width={20}
+          height={20}
+          alt="Download"
+        />
+        <p className="text-[1rem] font-semibold not-italic text-slate dark:text-white1">
+          Resume
+        </p>
+      </button>
+    </>
   );
 };
 
--- components/MobileDownload.tsx
+++ components/MobileDownload.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Resume } from "./svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MobileDownload: React.FC = () => {
+  const notify = () => toast("Downloaded Austin's Resume!");
+
   const downloadResume = () => {
     const link = document.createElement("a");
     link.href = `${window.location.origin}/AustinsResume.pdf`;
@@ -13,21 +17,27 @@ const MobileDownload: React.FC = () => {
 
     // Remove the link from the document
     document.body.removeChild(link);
+
+    // Show a toast notification
+    notify();
   };
 
   return (
-    <button
-      onClick={downloadResume}
-      className="flex w-[10rem] content-center justify-center gap-[.1875rem] rounded-[.33rem] bg-blue p-[.25rem] dark:bg-blue1 sm:w-[15rem] sm:p-[.5rem]"
-    >
-      <Resume
-        className="my-auto fill-white"
-        width={20}
-        height={20}
-        alt="Download"
-      />
-      <p className="boldbody sm:boldp text-white1 underline">Resume</p>
-    </button>
+    <>
+      <ToastContainer />
+      <button
+        onClick={downloadResume}
+        className="flex w-[10rem] content-center justify-center gap-[.1875rem] rounded-[.33rem] bg-blue p-[.25rem] dark:bg-blue1 sm:w-[15rem] sm:p-[.5rem]"
+      >
+        <Resume
+          className="my-auto fill-white"
+          width={20}
+          height={20}
+          alt="Download"
+        />
+        <p className="boldbody sm:boldp text-white1 underline">Resume</p>
+      </button>
+    </>
   );
 };
 
