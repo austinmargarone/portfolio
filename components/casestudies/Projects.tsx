069f42047e510600a3ce73fe7b6bcc2e05139fc6
fcvirtual <austin@fcvt.dev>
2024-03-18 13:05:22 -0400

    fix: Case study linking

--- components/casestudies/Projects.tsx
+++ components/casestudies/Projects.tsx
@@ -9,11 +9,11 @@ export default async function Projects(cases: any) {
         key={cases._id}
         className="casemain sm:casemainlg mx-auto sm:mb-[2.5rem]"
       >
-        <div
-          style={{ backgroundColor: cases.bg }}
-          className=" mx-auto flex w-[20rem] shrink-0 flex-col rounded-[.62725rem] border border-black bg-blue1 pb-[.32rem] pt-[2.83rem] hover:shadow-lg xs:w-[21.5rem] ss:w-[25rem] sm:h-[20.8rem] sm:w-[34.375rem] sm:pt-[4.25rem]"
-        >
-          <Link href={cases.case_study} className="mx-auto">
+        <Link href={cases.case_study} className="mx-auto">
+          <div
+            style={{ backgroundColor: cases.bg }}
+            className=" mx-auto flex w-[20rem] shrink-0 flex-col rounded-[.62725rem] border border-black bg-blue1 pb-[.32rem] pt-[2.83rem] hover:shadow-lg xs:w-[21.5rem] ss:w-[25rem] sm:h-[20.8rem] sm:w-[34.375rem] sm:pt-[4.25rem]"
+          >
             <div className="mx-auto flex sm:hidden">
               <div>
                 <Image
@@ -33,8 +33,6 @@ export default async function Projects(cases: any) {
                 />
               </div>
             </div>
-          </Link>
-          <Link href={cases.case_study} className="mx-auto">
             <div className="mx-auto hidden sm:flex">
               <div>
                 <Image
@@ -54,10 +52,8 @@ export default async function Projects(cases: any) {
                 />
               </div>
             </div>
-          </Link>
-        </div>
-        <div className="mx-auto flex justify-between pl-[1.25rem] pt-[1.22rem] sm:pl-[3.6rem] sm:pt-[1.95rem]">
-          <Link href={cases.case_study}>
+          </div>
+          <div className="mx-auto flex justify-between pl-[1.25rem] pt-[1.22rem] sm:pl-[3.6rem] sm:pt-[1.95rem]">
             <div className="flex-col">
               <h4 className="boldp sm:projectstitlelg flex dark:text-white">
                 {cases.title}
@@ -66,8 +62,7 @@ export default async function Projects(cases: any) {
                 {cases.description}
               </p>
             </div>
-          </Link>
-          <div className="flex">
+            {/* <div className="flex">
             <Link href={cases.case_study}>
               <button className="mr-[.5rem] mt-[.5rem] rounded-sm bg-blue px-[.5rem] py-[.2rem] hover:shadow-lg dark:bg-blue1 sm:mr-[1.5rem] sm:mt-[.5rem] sm:px-[1rem] sm:py-[.66rem]">
                 <p className="hidden text-white dark:text-black sm:block">
@@ -78,8 +73,9 @@ export default async function Projects(cases: any) {
                 </p>
               </button>
             </Link>
+          </div> */}
           </div>
-        </div>
+        </Link>
       </div>
     </>
   );
